99143b7b5a2abcc6027cb4e54642b43b97ccf0dd
kaiorod <caiorodrigo2775@gmail.com>
2021-08-15 22:31:27 -0300

    finaliza contagem

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace contagem_regressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("\nPreparar...");
+            Console.ReadKey();
+            
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("\nApontar...");
+            Console.ReadKey();
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("\nFOGO !");
+            Console.WriteLine();
+
+            Console.ResetColor();
+        }
+    }
+}
